2c7ecff779addc138345588fc2b84e784ad89265
Rishi Ranjan <101rishidsr@gmail.com>
2024-08-28 16:04:16 +0530

    done

--- api_calling/src/App.tsx
+++ api_calling/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+
+
+// useEffect is a hook that allows you to perform side effects in function components
+import React, { useState, useEffect } from 'react';
+
+// axios
+import axios from 'axios';
+
+interface TodoItem {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  // fetch
+  // const [todos, setTodos] = useState<TodoItem[]>([]);
+ 
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //   .then((response)=> response)
+  //   .then((e) => e.json())
+  //   .then(e => setTodos(e as TodoItem[]))
+     
+  // }, []);
+  
+  // axios
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  useEffect(() => {
+    axios.get<TodoItem[]>('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => setTodos(response.data))
+  }, []);
+
+
+
+  return (<div className='App'>
+    {todos.map((todo) =>
+      (<li key={todo.id}>{todo.title}</li>
+
+  ))
+  }
+  </div>
+     
+  )
+};
 
 export default App;
